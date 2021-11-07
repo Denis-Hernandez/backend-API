4ba9f519fc6db8291aa36ebc2d6f289711700491
Denis Hernandez <kandor95@gmail.com>
2021-11-06 21:52:56 -0600

    editando routs para consumir API de sistema Solar

--- routers/planets.router.js
+++ routers/planets.router.js
@@ -1,30 +1,29 @@
 const express = require('express');
+const fetchData = require('../utils/fetchData');
+
+const API = 'https://api.le-systeme-solaire.net/rest/bodies?filter[]=englishName,eq,'
+const DATA = '&data=gravity,density,moons,rel,vol,volValue,volExponent,meanRadius,mass,massValue,massExponent'
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-  res.json([
-    {
-      "planeta":"mercurio",
-      "masa":"xxxxx"
-    },
-    {
-      "planeta":"venus",
-      "masa":"xxxxx"
-    },
-    {
-      "planeta":"mercurio",
-      "masa":"xxxxx"
-    },
-    {
-      "planeta":"tierra",
-      "masa":"xxxxx"
-    },
-    {
-      "planeta":"marte",
-      "masa":"xxxxx"
-    }
-  ]);
+router.get('/',async (req,res)=>{
+  await fetchData(API)
+  .then(data=>{
+    res.json(data.bodies);
+  })
+});
+
+router.get('/:id',async (req,res)=>{
+  try {
+    const { id } = req.params;
+  await fetchData(API+id+DATA)
+  .then(data=>{
+    res.json(data);
+  })
+  } catch (error) {
+    console.log(error.message)
+    res.json(error.message)
+  }
 });
 
 module.exports = router;
